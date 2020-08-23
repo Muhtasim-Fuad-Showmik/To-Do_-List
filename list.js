e822919d4e017593ead7500ecf5c3a1bfb866ad5
Fuad-Hellscream <49075537+Fuad-Hellscream@users.noreply.github.com>
2020-08-24 01:45:09 +0600

    Organized Code and added a delete item function into the Javascript
    The earlier code did not have a delete items feature for the todos list. Added that into our script and organized the code to get rid of all the clutter by sectioning off each feature into separate functions.

--- list.js
+++ list.js
@@ -3,13 +3,14 @@ var todos = [];
 window.setTimeout(function() {
 	var input = prompt("What would you like to do?");
 
+		// Loop to keep prompting user to submit desired input
 		while(input!=="quit"){
 		if(input === "list"){
-			console.log(todos);
+			listTodos();
 		} else if(input === "new"){
-			var newTodo = prompt("Enter new todo");
-
-			todos.push(newTodo);
+			addTodo();
+		} else if(input === "delete"){
+			deleteTodo();
 		}
 
 		input = prompt("What would you like to do?");
@@ -17,3 +18,28 @@ window.setTimeout(function() {
 
 	console.log("OK, YOU QUIT THE APP");
 }, 500);
+
+// Function to list all items of the Todos list
+function listTodos(){
+	console.log("**********");
+	todos.forEach( function(todo, i){
+			console.log(i + ": " + todo);
+		});
+	console.log("**********");
+}
+
+// Function to add an item to the Todo List
+function addTodo(){
+	var newTodo = prompt("Enter new todo");
+
+	todos.push(newTodo);
+	console.log("Added Todo");
+}
+
+// Function to delete an item from the Todo List
+function deleteTodo(){
+	var index = prompt("Enter index of todo to delete");
+
+	todos.splice(index, 1);
+	console.log("Deleted Todo");
+}
